d1caeae7ab29a35fe854714203763618386c30d1
matheo.dalmasso <matheo.dalmasso@info0402.ipa.iutlens.local>
2024-11-04 13:43:15 +0100

    Fixes #9 - Creation de la fonction principale

--- AmazonJustePrix/app.py
+++ AmazonJustePrix/app.py
@@ -1,11 +1,38 @@
-from flask import Flask
+import sqlite3
+from wsgiref.validate import validator
 
+from flask import Flask, render_template
+import requests
+from flask_wtf import FlaskForm
+from wtforms.fields.numeric import FloatField
+from wtforms.validators import DataRequired
+
+con = sqlite3.connect('justePrix.db', check_same_thread=False)
 app = Flask(__name__)
+app.secret_key = 'secret'
 
+image = ""
+prix = 0
+nom = ""
 
-@app.route('/')
+class JustePrix (FlaskForm):
+    prixArticle = FloatField('Prix de l\'article', validators=[DataRequired()])
+
+@app.route('/', methods=['GET', 'POST'])
 def justePrixAmazon():  # put application's code here
+    global image,prix, nom
+    result = ""
 
+    form = JustePrix()
+
+    if form.validate_on_submit():
+        if form.prix_article.data == prix:
+            result = "Bravo, vous avez trouvé le juste prix !"
+        elif form.prix_article.data > prix:
+            result = "Le prix est trop grand"
+        else:
+            result = "Le prix est trop petit"
+    return render_template('game.html',image=image, form=form, prix=prix, nom=nom, result=result)
 
 
 if __name__ == '__main__':
