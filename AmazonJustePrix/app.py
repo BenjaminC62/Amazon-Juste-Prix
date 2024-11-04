86f2c1595e010d95208ae4b40f5055cd94d1608b
mathias_morel <mathias_morel@ens.univ-artois.fr>
2024-11-04 13:03:56 +0100

    Fixes #6 - Creation de la BD

--- AmazonJustePrix/app.py
+++ AmazonJustePrix/app.py
@@ -1,3 +1,5 @@
+import sqlite3
+
 from flask import Flask
 
 app = Flask(__name__)
@@ -6,6 +8,19 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():  # put application's code here
     return 'Hello World!'
+
+
+def creation_bd():
+    try:
+        conn = sqlite3.connect('justePrix.db')
+        cursor = conn.cursor()
+        cursor.execute('''CREATE TABLE ARTICLE(id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, nom_article TEXT NOT NULL, prix_article FLOAT NOT NULL, ref_article TEXT NOT NULL)''')
+        conn.commit()
+        conn.close()
+    except sqlite3.OperationalError:
+        print("La table existe déjà")
+
+creation_bd()
 
 
 if __name__ == '__main__':
